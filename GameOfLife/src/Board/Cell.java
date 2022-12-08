91d75f7ac20d75f6f3e16da6aa660be378706e18
C L <cedricivo.luechinger@uzh.ch>
2022-12-08 14:25:56 +0100

    basic logic added

--- GameOfLife/src/Board/Cell.java
+++ GameOfLife/src/Board/Cell.java
@@ -1,4 +1,32 @@
 package Board;
 
 public class Cell {
+
+    private boolean aIsAlive;
+    private ColorType aColor;
+
+    public Cell(){
+        aIsAlive = false;
+        aColor = ColorType.WHITE;
+    }
+
+    public void setColor(ColorType x){
+        aColor = x;
+    }
+
+    public ColorType getColor(){
+        return this.aColor;
+    }
+
+    public boolean isAlive(){
+        return aIsAlive;
+    }
+
+    public void kill(){
+        aIsAlive = false;
+    }
+
+    public void revive(){
+        aIsAlive = true;
+    }
 }
--- GameOfLife/src/Board/Grid.java
+++ GameOfLife/src/Board/Grid.java
@@ -1,4 +1,108 @@
 package Board;
 
+import java.sql.Array;
+
 public class Grid {
+
+    private Cell[][] aGrid;
+    private Cell[][] aNextGrid;
+    private final int aDimension;
+
+    public Grid(int pDimension){
+        aDimension = pDimension;
+        aGrid = new Cell[aDimension][aDimension];
+        aNextGrid = new Cell[aDimension][aDimension];
+        this.initGrid();
+    }
+
+    //TODO: getColor from cell and player
+    public void killACell(Cell pCell){
+        if(!pCell.isAlive() && pCell.getColor()==ColorType.WHITE){
+           throw new IllegalArgumentException("Please select a valid cell!");
+        }
+        pCell.kill();
+    }
+
+    public void reviveACell(Cell pCell){
+        if(pCell.isAlive()){
+            throw new IllegalArgumentException("Please select a dead cell!");
+        }
+        pCell.revive();
+    }
+
+
+    // TODO: Try less branches
+    // TODO: # of alive Cells from a specific player is not done yet
+    public void createNextGeneration(){
+        for(int i = 0; i < aDimension;i++) {
+            for (int j = 0; j < aDimension;j++) {
+                if(i == 0|| i == aDimension-1 || j == 0 || j == aDimension-1){
+                    aNextGrid[i][j] = aGrid[i][j];
+                } else {
+                    int numberOfNeighbors = countNeighbors(i, j);
+                    ColorType dominator = getDominantColor(i, j);
+
+                    if (!aGrid[i][j].isAlive() && numberOfNeighbors == 3) {
+                        aNextGrid[i][j] = aGrid[i][j];
+                        aNextGrid[i][j].revive();
+                        aNextGrid[i][j].setColor(dominator);
+                    } else if (aGrid[i][j].isAlive() && (numberOfNeighbors < 2 || numberOfNeighbors > 3)) {
+                        aNextGrid[i][j] = aGrid[i][j];
+                        aNextGrid[i][j].kill();
+                    } else {
+                        aNextGrid[i][j] = aGrid[i][j];
+                    }
+                }
+            }
+        }
+        aGrid = aNextGrid;
+    }
+
+    private int countNeighbors(int x, int y){
+        int numberOfNeighbors = 0;
+
+        for(int i = -1; i<2;i++) {
+            for (int j = -1; j < 2; j++) {
+                int col = (x+i) % aDimension;
+                int row = (y+j) % aDimension;
+
+                if(col != 0 && row != 0 && aGrid[col][row].isAlive()){
+                    numberOfNeighbors += 1;
+                }
+            }
+        }
+        return numberOfNeighbors;
+    }
+
+    private ColorType getDominantColor(int x, int y){
+        int cntBlue = 0;
+        int cntRed = 0;
+
+        for(int i = -1; i < 2;i++) {
+            for (int j = -1; j < 2; j++) {
+                int col = (x+i) % aDimension;
+                int row = (y+j) % aDimension;
+
+                if(i!=0 && j != 0 && aGrid[col][row].getColor() == ColorType.ROYALBLUE){
+                    cntBlue++;
+                }
+                if(i!=0 && j != 0 && aGrid[col][row].getColor() == ColorType.LAVARED){
+                    cntRed++;
+                }
+            }
+        }
+
+        if(cntRed>cntBlue){
+            return ColorType.LAVARED;
+        }
+        return ColorType.ROYALBLUE;
+    }
+
+    private void initGrid(){
+        for(int i = 0; i<aDimension;i++){
+            for(int j = 0; j<aDimension;j++){
+                aGrid[i][j] = new Cell();
+            }
+        }
+    }
 }
