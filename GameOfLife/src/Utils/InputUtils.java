d78e4a21ae35f6a0d47b9f30dbca39ff97e6ee13
Chopstew <patrickwidmer@gmx.ch>
2022-12-08 11:49:38 +0100

    Added doSleep and cleanString methods to InputUtils

--- GameOfLife/src/Utils/InputUtils.java
+++ GameOfLife/src/Utils/InputUtils.java
@@ -1,4 +1,24 @@
 package Utils;
 
+import java.util.concurrent.TimeUnit;
+
 public class InputUtils {
+
+    private final int aSecond;
+    public InputUtils(int sleep){
+        aSecond = sleep;
+    }
+
+    public void doSleep(){
+        try{
+            TimeUnit.SECONDS.sleep(this.aSecond);
+        }catch (InterruptedException e){
+            System.out.print("Please do not interrupt!\n");
+        }
+    }
+
+    public static boolean cleanString(String string){
+        if (string == null || string.isEmpty() || string.contains(" ")){return false;}
+        return true;
+    }
 }
