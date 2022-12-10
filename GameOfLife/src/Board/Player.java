901e53a5b69234409041982608478f4c3ce21f2c
Adrian <adrianfrancis.heiss@uzh.ch>
2022-12-10 14:03:37 +0100

    implemented player sorting by whole name instead of first char only. Added it to disableAll().

--- GameOfLife/src/Board/Player.java
+++ GameOfLife/src/Board/Player.java
@@ -1,9 +1,6 @@
 package Board;
 
-import Gui.SingletonGUI;
-import com.sun.tools.javac.Main;
-
-import java.awt.*;
+import static java.lang.Math.min;
 
 public class Player implements Comparable{
 
@@ -23,16 +20,25 @@ public class Player implements Comparable{
     public ColorType getPlayerColor(){return PLAYER_COLOR;}
     public int getCellCnt(){return cellCnt;}
 
-    public Character getPlayerInitial(){return PLAYER_NAME.charAt(0);}
+    public Character getPlayerInitial(){return Character.toUpperCase(PLAYER_NAME.charAt(0));}
 
     @Override
     public int compareTo(Object other) {
         if (other == null){return 0;}
         if (this.getClass() != other.getClass()){return 0;}
         Player otherPlayer = (Player) other;
-        if (this.getPlayerInitial() < otherPlayer.getPlayerInitial()){return -1;}
-        if (this.getPlayerInitial() > otherPlayer.getPlayerInitial()){return 1;}
-        return 0;
+        int minNameSize = min(this.getPlayerName().length(), otherPlayer.getPlayerName().length());
+        for (int i = 0; i < minNameSize; i++) {
+            char currCharThis = Character.toUpperCase(this.PLAYER_NAME.charAt(i));
+            char currCharOther = Character.toUpperCase(otherPlayer.PLAYER_NAME.charAt(i));
+            if(currCharThis < currCharOther){return -1;}
+            if(currCharThis > currCharOther){return 1;}
+        }
+        if (this.PLAYER_NAME.length() == minNameSize) {return -1;}
+        else {return 1;}
+        //if (this.getPlayerInitial() < otherPlayer.getPlayerInitial()){return -1;}
+        //if (this.getPlayerInitial() > otherPlayer.getPlayerInitial()){return 1;}
+        //return 0;
     }
 
     @Override
