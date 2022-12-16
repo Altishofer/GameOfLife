7127c9d3e6c24a52c4fd66c5fb30c228bc86f9cc
sandrin <sandrin@hunkeler.ws>
2022-12-16 09:51:15 +0100

    add tests

--- GameOfLife/test/Board/CellTest.java
+++ GameOfLife/test/Board/CellTest.java
@@ -1,5 +1,11 @@
 package Board;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class CellTest {
 
     @Test
@@ -11,7 +17,6 @@ class CellTest {
         assertTrue(targetCell.isAlive());
         assertEquals(ColorType.BLUE, targetCell.getColor());
     }
-
 
     @Test
     public void testGetColor() {
---
+++ GameOfLife/test/Board/ColorTypeTest.java
@@ -0,0 +1,7 @@
+package Board;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ColorTypeTest {
+
+}
