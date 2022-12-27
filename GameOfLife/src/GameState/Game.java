6fe0f4607a7f79995618fa146bf0acf17f388043
C L <cedricivo.luechinger@uzh.ch>
2022-12-27 15:23:18 +0100

    GameState hightes level

--- GameOfLife/src/GameState/Game.java
+++ GameOfLife/src/GameState/Game.java
@@ -2,13 +2,12 @@ package GameState;
 
 import Board.ColorType;
 import Board.Grid;
-import Board.Player;
 import Gui.SingletonGUI;
 
 import javax.swing.*;
 
 public class Game implements GameState {
-    private final Initialization aInitialisation;
+    private final GameState aInitialization;
     private final GameState aKill;
     private final GameState aRevive;
     private GameState aGameState;
@@ -19,10 +18,10 @@ public class Game implements GameState {
     public Game(Grid newGrid, SingletonGUI pGui, int pInitialCellCount){
         aGrid = newGrid;
         aGui = pGui;
-        aInitialisation = new Initialization(this, pInitialCellCount);
+        aInitialization = new Initialization(this, pInitialCellCount);
         aKill = new Kill(this);
         aRevive = new Revive(this);
-        aGameState = aInitialisation;
+        aGameState = aInitialization;
         Runnable r = SingletonGUI::getInstance;
         SwingUtilities.invokeLater(r);
         aEvolutionCount = 0;
